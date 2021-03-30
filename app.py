d328b358de4ce27c5e284257019c2424b9a376d1
Liz Lau <eklau@uiowa.edu>
2021-03-30 14:30:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@
 def home():
    return flask.render_template('estimate.html')
 
-@app.route('/get_numbers')
 def get_values():
    value1 = flask.request.args.get('val1')
    value2 = flask.request.args.get('val2')
-   return flask.jsonify({'data':f'<p>The result is: {
-       (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 25 +
-        (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 15}</p>'})
-        
+   value3 = (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 25 +
+        (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 15}
+   return flask.jsonify({'data':f'<p>The result is: {value3} <p>}) 
+   
+  
