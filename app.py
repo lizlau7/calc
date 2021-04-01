fa7d79a8f58f059227a14514367a2b0f19bd99d5
Liz Lau <eklau@uiowa.edu>
2021-04-01 13:01:17 -0500

    app

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ def home():
 def get_values():
    value1 = flask.request.args.get('val1')
    value2 = flask.request.args.get('val2')
-   value3 = (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 25 +
-        (2 * 3.14 * (value2*value1)+ (3.14 *value2^2) / 144) * 15}
-   return flask.jsonify({'data':f'<p>The result is: {value3} <p>}) 
+ 
    
   
