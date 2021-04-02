89905349fc64834be4f7d8ef0e8c44471cbcc922
Liz Lau <eklau@uiowa.edu>
2021-04-02 08:23:27 -0500

    calc folder delete

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import flask
-from flask import Flask
+from flask import Flask,request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-   return flask.render_template('estimate.html')
+def index():
+   return rendern_template('index.html')
+
+@app.route('/estimate')
+def estimate():
+   return render_template('estimate.html')
 
 def get_values():
    value1 = flask.request.args.get('val1')
    value2 = flask.request.args.get('val2')
  
-   
+
   
