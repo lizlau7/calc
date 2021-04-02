53cd24e37a8fb8c650dfd7584b309943836fd657
Liz Lau <eklau@uiowa.edu>
2021-04-01 19:41:26 -0500

    update

--- app.py
+++ app.py
@@ -2,18 +2,13 @@ import flask
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/estimate')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-   return ('estimate.html')
-if __name__=='__main__':
-   app.run('localhost',4999)
-
+   return flask.render_template('estimate.html')
 
 def get_values():
    value1 = flask.request.args.get('val1')
    value2 = flask.request.args.get('val2')
  
- @app.route('/About')
- def about():
-    return flask.render_template('About.html')
+   
+  
