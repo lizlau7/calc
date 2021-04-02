d744128fdcd02ac067362af4f7d6660820ba613f
Liz Lau <eklau@uiowa.edu>
2021-04-02 08:39:24 -0500

    templates update

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-   return flask.render_template('index.html')
-
-@app.route('/estimate')
-def estimate():
    return flask.render_template('estimate.html')
+
+@app.route('/Estimate.html')
+def estimate():
+   return flask.render_template('Estimate.html')
 
 def get_values():
    value1 = flask.request.args.get('val1')
