e6f02842cc047f2c0af1379f29ecc84dab87fec9
Liz Lau <eklau@uiowa.edu>
2021-04-06 09:36:01 -0500

    about

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import flask
 from flask import Flask,request, render_template
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/estimate', methods=['GET', 'POST'])
 def estimate():
    return flask.render_template('estimate.html')
 
-@app.route('/templates/about')
+@app.route('/')
+def index():
+   return flask.render_template('index.html')
+
+@app.route('/about')
 def about():
-   return flask.render_template('About.html')
+   return flask.render_template('about.html')
+
 
 def get_values():
    value1 = flask.request.args.get('val1')
