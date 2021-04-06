a82a8c7789051ee036e0d7baf75a3edf055c18dd
Liz Lau <eklau@uiowa.edu>
2021-04-06 07:55:39 -0500

    app update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def estimate():
-   return flask.render_template('estimate.html')
+   return flask.render_template('index.html')
 
 @app.route('/templates/about')
 def about():
