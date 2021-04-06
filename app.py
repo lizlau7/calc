a7e29803bbc6d6d4ee50f11cce79438d47b9a2a1
Liz Lau <eklau@uiowa.edu>
2021-04-06 08:34:38 -0500

    reset

--- app.py
+++ app.py
@@ -10,15 +10,6 @@ def estimate():
 def about():
    return flask.render_template('About.html')
 
-@app.route('/templates/estimate')
-def about():
-   return flask.render_template('estimate.html')
-
-@app.route('/templates/home')
-def about():
-   return flask.render_template('Home.html')
-
-
 def get_values():
    value1 = flask.request.args.get('val1')
    value2 = flask.request.args.get('val2')
